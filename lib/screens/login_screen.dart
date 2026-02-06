4ce955ed961f7cccf8c8173c2566a2b7c0285bb6
Nr0000003 <nr0010000003@gmail.com>
2026-02-06 12:43:17 -0600

    feat(rive): animation added

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: RiveAnimation.asset(
+                'assets/animated_login_bear.riv',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
