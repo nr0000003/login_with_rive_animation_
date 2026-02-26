888b22430f1cdd300484a6be90352e6f5d3c0fd0
Nr0000003 <nr0010000003@gmail.com>
2026-02-26 10:58:49 -0600

    (login): add typing debounce and map numLook

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
+import 'dart:async'; //3.1 importa el timer para simular la autenticación
+
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,145 +10,160 @@ class LoginScreen extends StatefulWidget {
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
-  //control para mostrar u ocultar la contraseña
-  bool _obscureText = true;
-  
-  //Crear el cerebrro de la animacion 
-  StateMachineController? _controller;
-  //SMI: State Machine Input
-  SMIBool? _isChecking;
-  SMIBool? _isHandsUp;
-  SMITrigger? _trigSuccess;
-  SMITrigger? _trigFail;
 
- //1))Crear Variables para Focusmode
+class _LoginScreenState extends State<LoginScreen> {
+//crear variable para mostrar u ocultar la contraseña
+  bool _obscureText = true;
+  //SMI: es una clase que se utiliza para controlar la animación de Rive desde el código de Flutter, es el cerebro de la animación de Rive, es decir, es el encargado de controlar las variables y los triggers de la animación de Rive desde el código de Flutter
+  //crear el cerebro de la animación de Rive
+  StateMachineController? _controller;
+  //SMI: State Machine Input: es una variable que se utiliza para controlar la animación de Rive desde el código de Flutter
+  SMIBool? _isChecking; //variable para controlar la animación de Rive cuando el usuario está escribiendo en el campo de email
+  SMIBool? _isHandsUp; //variable para controlar la animación de Rive cuando el usuario está escribiendo en el campo de password
+  SMITrigger? _trigSuccess; //variable para controlar la animación de Rive cuando el usuario hace clic en el botón de login
+  SMITrigger? _trigFail; //variable para controlar la animación de Rive cuando el usuario hace clic en el botón de login y la autenticación falla
+  SMINumber? _numLook; //variable para controlar la animación de Rive cuando el usuario mueve el mouse sobre el campo de email o password
+
+  //paso 1.1: crear variables para el focus de los campos de texto
   final _emailFocusNode = FocusNode();
   final _passwordFocusNode = FocusNode();
 
-  //2)) Listeners ()
+  //3.2 timer para dejar de escribir
+  Timer? _typingDebounce;
+  
+  //paso 1.2: Listenrs para FocusNodes (Oyentes/Chismosos)
   @override
   void initState() {
     super.initState();
     _emailFocusNode.addListener(() {
-      if (_emailFocusNode.hasFocus) {
-        //No tapes los ojos al ver email
-        if (_isHandsUp != null) {
-          _isHandsUp!.change(false);
+      if(_emailFocusNode.hasFocus) {
+        if(_isHandsUp != null){
+          //No tapes los ojos al ver email
+          _isHandsUp?.change(false);
+          //mirada neutral
+          _numLook?.value = 50.0;
         }
       }
+    }); 
+    _passwordFocusNode.addListener(() {
+      _isHandsUp?.change(_passwordFocusNode.hasFocus); //Levantar las manos al ver password
     });
-_passwordFocusNode.addListener(() {
-  // manos arriba en password
-  _isHandsUp?.change(_passwordFocusNode.hasFocus);
-});
-}
+  }
 
   @override
   Widget build(BuildContext context) {
-    //para obtener el tamaño de la pantalla y usarlo para ajustar el diseño
-    final Size size = MediaQuery.of(context).size;
- 
+    final size = MediaQuery.of(context).size;//obtenemos el tamaño de la pantalla para ajustar la animación y los campos de texto
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Padding( //agregamos un padding para que la animación y los campos de texto no estén pegados a los bordes de la pantalla
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),//padding horizontal de 20 para que la animación y los campos de texto no estén pegados a los bordes de la pantalla
           child: Column(
             children: [
-              SizedBox(
-                width: size.width,
-                height:250,
-              child: RiveAnimation.asset('assets/animated_login_bear.riv', 
-              stateMachines: ['Login Machine'],
-              //Al iniciar la animacion
-              onInit: (artboard) {
-                _controller = StateMachineController.fromArtboard(artboard, 'Login Machine');
-                //Verifica que inicio bien
-                if(_controller == null) return;
-                //Agrega el controlador al tablero/escenario
-                artboard.addController(_controller!);
-                //Vincular variables
-                _isChecking = _controller!.findSMI('isChecking');
-                _isHandsUp = _controller!.findSMI('isHandsUp');
-                _trigSuccess = _controller!.findSMI('trigSuccess');
-                _trigFail = _controller!.findSMI('trigFail');
-
-              }
-              
-              )
-              ),
-              //Para separacion
-              const SizedBox(height: 10),
+              Expanded(
+                child: RiveAnimation.asset('animated_login_bear.riv',//agregamos la animación de Rive en la parte superior de la pantalla
+                stateMachines:['Login Machine'],//especificamos el nombre de la máquina de estados que queremos utilizar para controlar la animación de Rive
+                onInit: (artboart){
+                  _controller = StateMachineController.fromArtboard(artboart, 'Login Machine');
+                  if(_controller == null) return;
+                  artboart.addController(_controller!);
+                  //vinculamos las variables de la animación de Rive con las variables de Flutter para poder controlar la animación desde el código de Flutter
+                  _isChecking = _controller!.findSMI('isChecking');
+                  _isHandsUp = _controller!.findSMI('isHandsUp');
+                  _trigSuccess = _controller!.findSMI('trigSuccess');
+                  _trigFail = _controller!.findSMI('trigFail');
+                  _numLook = _controller!.findSMI('numLook');
+                },
+                )),
+              const SizedBox(height: 10), //separación entre la animación y el campo de email
               TextField(
-                //3)asignar FocusNode al TextField
+                //paso 1.3: asociar los focusNodes a los campos de texto
                 focusNode: _emailFocusNode,
-                onChanged: (value) {
-                  if (_isHandsUp != null) {
+                onChanged: (value){
+                 //asociamos el focusNode al campo de email
+                  if(_isHandsUp != null){
                     //No tapes los ojos al ver email
                     //_isHandsUp!.change(false);
                   }
-                  if (_isChecking == null) return;
-                  //Activa  el modo chisme
-                  _isChecking!.change(true);
+                  //Si isChecking no es null
+                  if(_isChecking == null) return;
+
+                  //Activar modo chismoso
+                    _isChecking!.change(true);
+
+                    //2.4 implementar numlook
+                    //ajustes de limites de 0 a 100
+                    //80 como medida de calibración
+
+                    final look = (value.length/80.0*100.0).clamp(0.0, 100.0);//Clamp es el rango
+                    _numLook?.value = look;
+
+                    //3.3 Debounce: si vuelve a escribir, reinicia el timer
+                    _typingDebounce?.cancel();
+                    _typingDebounce = Timer(const Duration(seconds: 1), () {
+                      if(!mounted) return;
+                      //mirada neutra
+                      _isChecking?.change(false);
+                    });
                 },
-                //Para mostrar un tipo de tecleado
                 keyboardType: TextInputType.emailAddress,
                 decoration: InputDecoration(
                   hintText: 'Email',
-                  prefixIcon: Icon(Icons.email),
+                  prefixIcon: const Icon(Icons.email),//icono de email para el campo de email
                   border: OutlineInputBorder(
-                    //Para redondear los bordes del campo de texto
-                    borderRadius: BorderRadius.circular(12),
-                  ),
+                    borderRadius: BorderRadius.circular(12) //borde redondeado para el campo de email
+                  )
                 ),
               ),
-              const SizedBox(height: 10),
+              const SizedBox(height: 10), //separación entre el campo de email y el campo de password
               TextField(
-                //3)asignar FocusNode al TextField
+                //paso 1.3: asociar los focusNodes a los campos de texto
                 focusNode: _passwordFocusNode,
-                onChanged: (value) {
-                  if (_isHandsUp != null) {
-                    //No modo chisme
-                    _isChecking!.change(false);
+                onChanged: (value){
+                  if(_isChecking != null){
+                    //No quiero modo chismoso
+                    //_isChecking!.change(false);
                   }
-                  if (_isHandsUp == null) return;
-                  //Arriba las manos
-                  //_isHandsUp!.change(true);
+                  //Si isHandsUp no es null
+                  if(_isHandsUp == null) return;
+                  //Levantar las manos al ver password
+                    _isHandsUp!.change(true); 
                 },
-
-                obscureText: _obscureText,
+                obscureText: _obscureText, //ocultamos la contraseña por defecto  
                 decoration: InputDecoration(
                   hintText: 'Password',
-                  prefixIcon: const Icon(Icons.lock),//Cerrado o Seguro
+                  prefixIcon: const Icon(Icons.lock),//icono de candado para el campo de password
                   suffixIcon: IconButton(
-                    //if terniario
+                    //if ternario
                     icon: Icon(
-                      _obscureText ? Icons.visibility : Icons.visibility_off),
+                      _obscureText ? Icons.visibility : Icons.visibility_off, //cambiamos el icono de ojo dependiendo de si la contraseña está oculta o no
+                    ),
                     onPressed: () {
-                      //Refresca el widget para mostrar u ocultar la contraseña
+                      //refresca el icono
                       setState(() {
-                        //Cambiar el estado de _obscureText para mostrar u ocultar la contraseña
                         _obscureText = !_obscureText;
                       });
                     },
-                  ),
+                  ), 
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
+                    borderRadius: BorderRadius.circular(12)//borde redondeado para el campo de password
+                  )
                 ),
-              )
-            ],
+              ),
+              const SizedBox(height: 10), //separación entre el campo de password y el botón de login
+              ]
           ),
-        ),
+        )
       ),
     );
   }
-  //1.4 liberarr memoria/recursos al salir de la pantalla
+
+  //paso 1.4: limpiar los focusNodes para evitar fugas de memoria
   @override
   void dispose() {
-    //4) Liberar los FocusNode para evitar fugas de memoria
+    //limpiar los focusNodes para evitar fugas de memoria
     _emailFocusNode.dispose();
     _passwordFocusNode.dispose();
+    _typingDebounce?.cancel(); //cancelar el timer de debounce para evitar fugas de memoria
     super.dispose();
   }
 }
